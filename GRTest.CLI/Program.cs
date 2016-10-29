948e2e3f91c9a96cfd58d5e98e32327bbfabbf36
vamsi is awesome <vamsi@sportslock.com>
2016-10-28 21:50:26 -0500

    CLI implmented

--- GRTest.CLI/Program.cs
+++ GRTest.CLI/Program.cs
@@ -1,14 +1,56 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
-using System.Reflection;
+using System.Linq;
+using GRTest.Data.Models;
+using GRTest.Services;
 
 namespace GRTest.CLI
 {
     class Program
     {
+        //All files stored in /Data directory of this project
+        //To make it easy, we're just looking for any file in that directory
         static void Main(string[] args)
         {
-            var directory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var directoryPath = AppDomain.CurrentDomain.BaseDirectory + "/Data";
+            var filePaths = Directory.GetFiles(directoryPath);
+
+            var people = new List<Person>();
+
+            foreach (var filePath in filePaths)
+            {
+                try
+                {
+                    var data = File.ReadAllText(filePath);
+                    people.AddRange(new PersonParsingService().ParsePeopleData(data));
+                }
+                catch (Exception ex)
+                {
+                    //simply output any errors we get
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            PrintSet(people.OrderBy(c => c.Gender).ThenBy(c => c.LastName), "Ordered by Gender then Last Name");
+            PrintSet(people.OrderBy(c => c.DateOfBirth), "Ordered by Birth Date");
+            PrintSet(people.OrderByDescending(c => c.LastName), "Ordered by Last Name descending");
+
+            //wait for response to exit
+            Console.WriteLine("Press any key to exit");
+
+            Console.ReadKey();
+        }
+
+        static void PrintSet(IEnumerable<Person> dataSet, string title)
+        {
+            //add a new line just for separation
+            Console.WriteLine();
+            Console.WriteLine(title);
+            foreach (var person in dataSet)
+            {
+                Console.WriteLine($"{person.FirstName} {person.LastName}, {person.Gender}, Favorite Color: {person.FavoriteColor}, DOB: {person.DateOfBirth.ToString("M/d/yyyy")}");
+            }
         }
     }
 }
